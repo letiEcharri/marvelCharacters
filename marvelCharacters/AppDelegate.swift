73f0295ad330fb4dc67d023a6c059bf2093f43d6
Leticia <leticia.echarri@gmail.com>
2021-12-20 12:39:41 +0100

    Create project

---
+++ marvelCharacters/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  marvelCharacters
+//
+//  Created by Leticia Echarri on 20/12/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+}
+
---
+++ marvelCharacters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  marvelCharacters
+//
+//  Created by Leticia Echarri on 20/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
